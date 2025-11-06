ccb859c0687de969a031844d4c51ce59b29a7418
ikerrios9 <1925184@alu.murciaeduca.es>
2025-11-06 21:08:58 +0100

    extra

--- demo/src/main/java/com/cipriano/demo/repo/UsuarioRepository.java
+++ demo/src/main/java/com/cipriano/demo/repo/UsuarioRepository.java
@@ -2,5 +2,7 @@ package com.cipriano.demo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.cipriano.demo.domain.Usuario;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
