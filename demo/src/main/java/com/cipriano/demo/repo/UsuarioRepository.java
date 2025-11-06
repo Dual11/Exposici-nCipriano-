8a5163bf4bee72f22941d7229b0348b6182e9d17
ikerrios9 <1925184@alu.murciaeduca.es>
2025-11-06 21:14:43 +0100

    borrado

--- demo/src/main/java/com/cipriano/demo/repo/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cipriano.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cipriano.demo.domain.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
