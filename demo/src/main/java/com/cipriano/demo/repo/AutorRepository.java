b9ac7bb9359843ecaf6cd75889d9c06aca68846f
ikerrios <1925184@alu.murciaeduca.es>
2025-11-06 21:15:24 +0100

    Delete demo directory

--- demo/src/main/java/com/cipriano/demo/repo/AutorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cipriano.demo.repo;
-
-import com.cipriano.demo.domain.Autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutorRepository extends JpaRepository<Autor, Long> {
-}
--- demo/src/main/java/com/cipriano/demo/repo/CategoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cipriano.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cipriano.demo.domain.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
